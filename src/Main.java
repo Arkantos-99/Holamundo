11da79392243c70e603efa0a85c7ac33f785170e
AlumnoFP <gastonismael99@gmail.com>
2024-10-14 10:38:05 +0200

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double dec = 20.24;
+        char letra = 'l';
+        int num = 2024;
+        String texto = "es el año";
+        System.out.println("Hola Mundo " + texto + " " + num);
+
+        if (num == 2024) {
+            System.out.println("es el año 2024");
+        } else if (num==2025){
+            System.out.println("no es el año 2024");
+        }
+
+    }
+}
